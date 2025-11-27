d4b294f648caf16b1dde8569361694bf4b6a89fa
Toy <119365718+toykubo@users.noreply.github.com>
2025-11-27 16:27:45 +0900

    20251127

--- src/main.rs
+++ src/main.rs
@@ -22,15 +22,17 @@ const FLAG_ZERO: u8 = 1 << 1; // Set if A = 0
 const FLAG_INTERRUPT: u8 = 1 << 2; // Not affected
 const FLAG_DECIMAL: u8 = 1 << 3; // Not affected
 const FLAG_BREAK: u8 = 1 << 4; // Not affected
+// 5 は未使用
 const FLAG_OVERFLOW: u8 = 1 << 6; // Set if sign bit is incorrect
 const FLAG_NEGATIVE: u8 = 1 << 7; // Set if bit 7 set
+
+const SIGN_BIT: u8 = 1 << 7;
 
 pub struct CPU {
     pub register_a: u8,
     pub register_x: u8,
     pub register_y: u8,
     pub status: u8,
-    // 5 は未使用
     pub program_counter: u16,
     pub memory: [u8; 0x10000], // 0xFFFF
 }
@@ -102,96 +104,6 @@ impl CPU {
         }
     }
 
-    fn sbc(&mut self, mode: &AddressingMode) {
-        // A-M-(1-C)
-        // キャリーかどうかの判定が逆
-        // キャリーの引き算(1-C)
-        // overflowの判定が逆 = m,p p,m
-        let addr = self.get_operand_address(mode);
-        let value = self.mem_read(addr);
-
-        let carry = self.status & FLAG_CARRY;
-        let (v1, carry_flag1) = self.register_a.overflowing_sub(value);
-        let (n, carry_flag2) = v1.overflowing_sub(1 - carry);
-
-        let overflow =
-            (self.register_a & 0x80) != (value & 0x80) && (self.register_a & 0x80) != (n & 0x80);
-
-        self.register_a = n;
-
-        self.status = if !carry_flag1 && !carry_flag2 {
-            self.status | FLAG_CARRY
-        } else {
-            self.status & !FLAG_CARRY
-        };
-
-        self.status = if overflow {
-            self.status | FLAG_OVERFLOW
-        } else {
-            self.status & !FLAG_OVERFLOW
-        };
-
-        self.update_zero_and_negative_flags(self.register_a);
-    }
-
-    fn adc(&mut self, mode: &AddressingMode) {
-        let addr = self.get_operand_address(mode);
-        let value = self.mem_read(addr);
-
-        let carry = self.status & FLAG_CARRY;
-        let (rhs, carry_flag1) = value.overflowing_add(carry);
-        let (n, carry_flag2) = self.register_a.overflowing_add(rhs);
-
-        let overflow = (self.register_a & 0x80) == (value & 0x80) && (value & 0x80) != (n & 0x80);
-
-        self.register_a = n;
-
-        self.status = if carry_flag1 || carry_flag2 {
-            self.status | FLAG_CARRY
-        } else {
-            self.status & !FLAG_CARRY
-        };
-
-        self.status = if overflow {
-            self.status | FLAG_OVERFLOW
-        } else {
-            self.status & !FLAG_OVERFLOW
-        };
-
-        self.update_zero_and_negative_flags(self.register_a);
-    }
-
-    fn lda(&mut self, mode: &AddressingMode) {
-        let addr = self.get_operand_address(mode);
-        let value = self.mem_read(addr);
-        self.register_a = value;
-        self.update_zero_and_negative_flags(self.register_a);
-    }
-
-    fn sta(&mut self, mode: &AddressingMode) {
-        let addr = self.get_operand_address(mode);
-        self.mem_write(addr, self.register_a);
-    }
-
-    fn tax(&mut self) {
-        self.register_x = self.register_a;
-        self.update_zero_and_negative_flags(self.register_x);
-    }
-
-    fn update_zero_and_negative_flags(&mut self, result: u8) {
-        self.status = if result == 0 {
-            self.status | FLAG_ZERO // Set zero flag
-        } else {
-            self.status & !FLAG_ZERO // Clear zero flag
-        };
-
-        self.status = if result & 0x80 != 0 {
-            self.status | FLAG_NEGATIVE // Set negative flag
-        } else {
-            self.status & !FLAG_NEGATIVE // Clear negative flag
-        }
-    }
-
     fn inx(&mut self) {
         self.register_x = self.register_x.wrapping_add(1);
         self.update_zero_and_negative_flags(self.register_x);
@@ -247,6 +159,18 @@ impl CPU {
             println!("OPS: {:X}", opscode);
 
             match opscode {
+                0x09 => {
+                    self.ora(&AddressingMode::Immediate);
+                    self.program_counter += 1;
+                }
+                0x49 => {
+                    self.eor(&AddressingMode::Immediate);
+                    self.program_counter += 1;
+                }
+                0x29 => {
+                    self.and(&AddressingMode::Immediate);
+                    self.program_counter += 1;
+                }
                 0xE9 => {
                     self.sbc(&AddressingMode::Immediate);
                     self.program_counter += 1;
@@ -332,11 +256,137 @@ impl CPU {
             }
         }
     }
-}
 
+    fn ora(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+        self.register_a = self.register_a | value;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn eor(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+        self.register_a = self.register_a ^ value;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn and(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+        self.register_a = self.register_a & value;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn sbc(&mut self, mode: &AddressingMode) {
+        // A-M-(1-C)
+        // キャリーかどうかの判定が逆
+        // キャリーの引き算(1-C)
+        // overflowの判定が逆 = m,p p,m
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+
+        let carry = self.status & FLAG_CARRY;
+        let (v1, carry_flag1) = self.register_a.overflowing_sub(value);
+        let (n, carry_flag2) = v1.overflowing_sub(1 - carry);
+
+        let overflow = (self.register_a & SIGN_BIT) != (value & SIGN_BIT)
+            && (self.register_a & 0x80) != (n & SIGN_BIT);
+
+        self.register_a = n;
+
+        self.status = if !carry_flag1 && !carry_flag2 {
+            self.status | FLAG_CARRY
+        } else {
+            self.status & !FLAG_CARRY
+        };
+
+        self.status = if overflow {
+            self.status | FLAG_OVERFLOW
+        } else {
+            self.status & !FLAG_OVERFLOW
+        };
+
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn adc(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+
+        let carry = self.status & FLAG_CARRY;
+        let (rhs, carry_flag1) = value.overflowing_add(carry);
+        let (n, carry_flag2) = self.register_a.overflowing_add(rhs);
+
+        let overflow = (self.register_a & 0x80) == (value & 0x80) && (value & 0x80) != (n & 0x80);
+
+        self.register_a = n;
+
+        self.status = if carry_flag1 || carry_flag2 {
+            self.status | FLAG_CARRY
+        } else {
+            self.status & !FLAG_CARRY
+        };
+
+        self.status = if overflow {
+            self.status | FLAG_OVERFLOW
+        } else {
+            self.status & !FLAG_OVERFLOW
+        };
+
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn lda(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+        self.register_a = value;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn sta(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        self.mem_write(addr, self.register_a);
+    }
+
+    fn tax(&mut self) {
+        self.register_x = self.register_a;
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    fn update_zero_and_negative_flags(&mut self, result: u8) {
+        self.status = if result == 0 {
+            self.status | FLAG_ZERO // Set zero flag
+        } else {
+            self.status & !FLAG_ZERO // Clear zero flag
+        };
+
+        self.status = if result & 0x80 != 0 {
+            self.status | FLAG_NEGATIVE // Set negative flag
+        } else {
+            self.status & !FLAG_NEGATIVE // Clear negative flag
+        }
+    }
+}
 #[cfg(test)]
 mod test {
     use super::*;
+
+    fn run<F>(program: Vec<u8>, f: F) -> CPU
+    where
+        F: Fn(&mut CPU),
+    {
+        let mut cpu = CPU::new();
+        cpu.load(program);
+        cpu.reset();
+        f(&mut cpu);
+        cpu.run();
+        cpu
+    }
+
+    fn assert_status(cpu: CPU, flags: u8) {
+        assert_eq!(cpu.status, flags)
+    }
 
     #[test]
     fn test_0xa9_lda_immediate_data() {
@@ -363,19 +413,24 @@ mod test {
 
     #[test]
     fn test_0xaa_tax_move_a_to_x() {
-        let mut cpu = CPU::new();
-        cpu.load(vec![0xaa, 0x00]);
-        cpu.reset();
-        cpu.register_a = 10;
-        cpu.run();
+        let cpu = run(vec![0xaa, 0x00], |cpu| {
+            cpu.register_a = 10;
+        });
+
+        // let mut cpu = CPU::new();
+        // cpu.load(vec![0xaa, 0x00]);
+        // cpu.reset();
+        // cpu.register_a = 10;
+        // cpu.run();
 
         assert_eq!(cpu.register_x, 10);
     }
 
     #[test]
     fn test_5_ops_working_together() {
-        let mut cpu = CPU::new();
-        cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+        let cpu = run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00], |_| {});
+        // let mut cpu = CPU::new();
+        // cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
 
         assert_eq!(cpu.register_x, 0xc1);
     }
@@ -663,13 +718,47 @@ mod test {
 
     #[test]
     fn test_sbc_no_overflow() {
-        let mut cpu = CPU::new();
-        cpu.load(vec![0xE9, 0x7F, 0x00]);
-        cpu.reset();
-        cpu.register_a = 0x7E;
-        cpu.status = 0x01;
-        cpu.run();
+        let cpu = run(vec![0xE9, 0x7F, 0x00], |cpu| {
+            cpu.register_a = 0x7E;
+            cpu.status = FLAG_CARRY;
+        });
         assert_eq!(cpu.register_a, 0xFF);
-        assert_eq!(cpu.status, 0x80);
+        assert_status(cpu, FLAG_NEGATIVE);
+
+        // let mut cpu = CPU::new();
+        // cpu.load(vec![0xE9, 0x7F, 0x00]);
+        // cpu.reset();
+        // cpu.register_a = 0x7E;
+        // cpu.status = 0x01;
+        // cpu.run();
+        // assert_eq!(cpu.register_a, 0xFF);
+        // assert_eq!(cpu.status, 0x80);
+    }
+
+    #[test]
+    fn test_and() {
+        let cpu = run(vec![0x29, 0x0C, 0x00], |cpu| {
+            cpu.register_a = 0x0A;
+        });
+        assert_eq!(cpu.register_a, 0x08);
+        assert_status(cpu, 0);
+    }
+
+    #[test]
+    fn test_eor() {
+        let cpu = run(vec![0x49, 0x0C, 0x00], |cpu| {
+            cpu.register_a = 0x0A;
+        });
+        assert_eq!(cpu.register_a, 0x06);
+        assert_status(cpu, 0);
+    }
+
+    #[test]
+    fn test_ora() {
+        let cpu = run(vec![0x09, 0x0C, 0x00], |cpu| {
+            cpu.register_a = 0x0A;
+        });
+        assert_eq!(cpu.register_a, 0x0E);
+        assert_status(cpu, 0);
     }
 }
